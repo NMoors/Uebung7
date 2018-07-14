eb2c3e65fd06c72fd3c83ab50e8f7c7d42b823bb
NMoors <nauka.moors@informatik.hs-fulda.de>
2018-07-14 20:31:48 +0200

    API del

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var request = require('request');
+//var request = require('request');
 
 app.use(express.static('HTML'));
 app.use(express.static('./'));
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/HTML/Index.html'));
-
-  request('https://api/location/search/?query=london', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  obj = JSON.parse(body);
-  console.log(obj.title);
-  console.log(obj);
-});
-
-res.send('/HTML/Kumo.html', {data: obj});
-
 });
 
 
